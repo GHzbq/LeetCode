a6c28aa672bfb5b7f71241bae6d62ac48f6a666e
章拔清 <eric_qiushui@126.com>
2021-04-20 21:02:02 +0800

    [add 28]

---
+++ sourceCode/cpp/lt000028/Solution.h
@@ -0,0 +1,23 @@
+//
+// Created by zhangbaqing on 2021/4/20.
+//
+
+#ifndef LEETCODE_SOLUTION_H
+#define LEETCODE_SOLUTION_H
+
+#include <string>
+
+using std::string;
+
+#if true
+
+class Solution {
+public:
+    int strStr(const string &haystack, const string &needle) {
+        return haystack.find(needle);
+    }
+};
+
+#endif
+
+#endif //LEETCODE_SOLUTION_H
